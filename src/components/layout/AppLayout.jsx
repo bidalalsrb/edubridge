fcf7c59f67d88e83f177a539383c3903f463601e
mg <bidal.mingyu@gmail.com>
2026-02-19 22:24:53 +0900

    높이 조정2

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -4,11 +4,11 @@ import { BottomNav } from '../home/BottomNav'
 
 export function AppLayout() {
   return (
-    <main className="app-compact min-h-screen bg-[#eff2f7] px-3 ">
-      <div className="mx-auto w-full lg:w-[60%]">
-        <div className="mx-auto w-full overflow-hidden rounded-[20px] border border-slate-200 bg-[#f3f5f9] shadow-[0_20px_55px_-35px_rgba(37,99,235,0.5)]">
+    <main className="app-compact bg-[#eff2f7] px-3">
+      <div className="mx-auto h-full w-full lg:w-[60%]">
+        <div className="mx-auto flex h-full w-full flex-col overflow-hidden rounded-[20px] border border-slate-200 bg-[#f3f5f9] shadow-[0_20px_55px_-35px_rgba(37,99,235,0.5)]">
           <AppHeader />
-          <div className="border-t border-slate-100 bg-[#f8f9fc]">
+          <div className="flex-1 overflow-y-auto border-t border-slate-100 bg-[#f8f9fc]">
             <Outlet />
           </div>
           <BottomNav />
